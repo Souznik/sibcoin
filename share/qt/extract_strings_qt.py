e6101d29e0f1eae9e5a0cc9806aa751fdd9e7d32
ivan <ivansib@users.noreply.github.com>
2015-05-02 12:49:50 +0200

    change translations

--- share/qt/extract_strings_qt.py
+++ share/qt/extract_strings_qt.py
@@ -8,7 +8,7 @@ import glob
 import operator
 import os
 
-OUT_CPP="src/qt/dashstrings.cpp"
+OUT_CPP="src/qt/sibcoinstrings.cpp"
 EMPTY=['""']
 
 def parse_po(text):
@@ -77,6 +77,6 @@ f.write('static const char UNUSED *dash_strings[] = {\n')
 messages.sort(key=operator.itemgetter(0))
 for (msgid, msgstr) in messages:
     if msgid != EMPTY:
-        f.write('QT_TRANSLATE_NOOP("dash-core", %s),\n' % ('\n'.join(msgid)))
+        f.write('QT_TRANSLATE_NOOP("sibcoin-core", %s),\n' % ('\n'.join(msgid)))
 f.write('};\n')
 f.close()
